ab958e47d5293eefbe5d465dd4a07a54a9f90a86
Henrique Garcia Manfio de Almeida <101586539+HenriqueGarc1a@users.noreply.github.com>
2026-08-06 00:44:19 +0000

    first

---
+++ src/parte_1/App.java
@@ -0,0 +1,13 @@
+public class App {
+ public static void main(String[] args) throws Exception {
+ System.out.println("\nHello!");
+ String version = System.getProperty("java.version");
+ System.out.println("Running Java Version "+version+"\n");
+ System.out.println(System.getProperty("file.separator"));
+ System.out.println(System.getProperty("java.class.path"));
+ System.out.println(System.getProperty("os.version"));
+ System.out.println(System.getProperty("user.name"));
+
+
+ }
+}
